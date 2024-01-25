f84366fe3e1416776f4f40fde814218e349b2c12
ad17427 <andoni.serrano@versia.com>
2024-01-25 18:24:31 +0100

    Payling fix

--- app/Http/Controllers/Admin/BookingController.php
+++ app/Http/Controllers/Admin/BookingController.php
@@ -142,7 +142,7 @@ class BookingController extends AppBaseController
     {
         $school = $this->getSchool($request);
         $booking = Booking::find($id);
-        $paymentMethod = $request->get('payment_method') ?? $booking->payment_method;
+        $paymentMethod = $request->get('payment_method') ?? $booking->payment_method_id;
 
 
         if (!$booking) {
