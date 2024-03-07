f8b79d1f03c2f7add6726fe3a5bbdfa547a4b13a
ad17427 <andoni.serrano@versia.com>
2024-03-07 14:12:34 +0100

    Api see booking

--- app/Http/Controllers/API/BookingAPIController.php
+++ app/Http/Controllers/API/BookingAPIController.php
@@ -14,6 +14,7 @@ use App\Models\User;
 use App\Repositories\BookingRepository;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 /**
