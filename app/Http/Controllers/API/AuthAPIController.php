f497c482ae54edb865533a7a2f4c59b4aa9ee39f
ad17427 <andoni.serrano@versia.com>
2024-10-17 09:43:32 +0200

    fix recover password

--- app/Http/Controllers/API/AuthAPIController.php
+++ app/Http/Controllers/API/AuthAPIController.php
@@ -36,27 +36,34 @@ class AuthAPIController extends AppBaseController
             'school_id' => 'required'
         ]);
 
+        // Busca el usuario a través de la relación con el cliente y las escuelas
         $user = User::where('email', $request->email)
             ->where('type', $request->type)
-            ->whereHas('schoolUsers', function ($q) use($request) {
-                $q ->where('school_id', $request->school_id);
-            })->first();
+            ->whereHas('clients', function ($query) use ($request) {
+                // Filtrar por las escuelas del cliente
+                $query->whereHas('schools', function ($schoolQuery) use ($request) {
+                    $schoolQuery->where('school_id', $request->school_id);
+                });
+            })
+            ->first();
 
         if (!$user) {
-            return response()->json(['email' => 'No podemos encontrar un usuario con ese email y tipo.']);
+            return response()->json(['email' => 'No podemos encontrar un usuario con ese email, tipo y escuela.'], 404);
         }
 
-        // Generar token
+        // Generar token de recuperación
         $token = Str::random(60);
 
+        // Guardar el token de recuperación en el usuario
         $user->recover_token = $token;
         $user->save();
 
-        // Enviar correo electrónico con el enlace
+        // Enviar correo electrónico con el enlace de recuperación
         Mail::to($user->email)->send(new RecoverPassword($user));
 
         return response()->json(['message' => 'Se ha enviado un enlace de restablecimiento de contraseña.']);
     }
+
 
     public function resetPassword(Request $request)
     {
