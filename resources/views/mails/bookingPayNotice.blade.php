67f5fc37ab20622a447809ebeb2375149988cd75
ad17427 <andoni.serrano@versia.com>
2024-10-24 19:54:41 +0200

    qr test

--- resources/views/mails/bookingPayNotice.blade.php
+++ resources/views/mails/bookingPayNotice.blade.php
@@ -22,7 +22,7 @@
         <tbody>
         <tr>
             <td align="center">
-                <img src="data:image/png;base64,{{$qrCodeBase64 = QrCode::format('png')->size(300)->generate($actionURL)}}" alt="QR Code" style="width: 110px; height: 110px;">
+                <img src="data:image/png;base64,{{QrCode::format('png')->size(300)->generate($actionURL)}}" alt="QR Code" style="width: 110px; height: 110px;">
             </td>
         </tr>
         </tbody>
