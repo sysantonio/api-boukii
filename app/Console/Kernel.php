8e5be56a1d7bba47073cd5c1df8879664cb489a7
ad17427 <andoni.serrano@versia.com>
2025-01-24 19:23:24 +0100

    Fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('Station:weatherForecast')
             ->hourly()
             ->runInBackground();
-        $schedule->job(new UpdateMonitorForSubgroup)->everyFiveMinutes()->runInBackground();
+        $schedule->job(new UpdateMonitorForSubgroup)->everyFiveMinutes();
 
 
     }
