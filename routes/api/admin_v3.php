4ad7a505ae554e9d0042e349db8fa63251ba6f8f
ad17427 <12254712+sysantonio@users.noreply.github.com>
2025-07-28 20:23:41 +0200

    boukii 5.0

--- routes/api/admin_v3.php
+++ routes/api/admin_v3.php
@@ -1,25 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\V3\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('dashboard')->group(function () {
-    Route::get('/summary', function () {
-        return response()->json(['message' => 'summary']);
-    });
 
-    Route::get('/courses', function () {
-        return response()->json(['message' => 'courses']);
-    });
-
-    Route::get('/weather', function () {
-        return response()->json(['message' => 'weather']);
-    });
-
-    Route::get('/sales', function () {
-        return response()->json(['message' => 'sales']);
-    });
-
-    Route::get('/reservations', function () {
-        return response()->json(['message' => 'reservations']);
-    });
+Route::prefix('dashboard')->controller(DashboardController::class)->group(function () {
+    Route::get('/summary', 'summary');
+    Route::get('/courses', 'courseStats');
+    Route::get('/weather', 'weather');
+    Route::get('/sales', 'sales');
+    Route::get('/reservations', 'reservations');
 });
+
