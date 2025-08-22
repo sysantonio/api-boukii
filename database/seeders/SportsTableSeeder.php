f64012e10a376c1857dfdc01baf42a9cb01fe1d3
sysantonio <andf92@gmail.com>
2025-08-22 21:26:05 +0200

    feat: seed default sports and salary levels

--- database/seeders/SportsTableSeeder.php
+++ database/seeders/SportsTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Sport;
+use App\Models\SportType;
 
 class SportsTableSeeder extends Seeder
 {
@@ -13,6 +15,32 @@ class SportsTableSeeder extends Seeder
      */
     public function run()
     {
+        $types = ['Hiver', 'Été', 'Autres'];
 
+        $typeIds = [];
+        foreach ($types as $typeName) {
+            $type = SportType::firstOrCreate(['name' => $typeName]);
+            $typeIds[$typeName] = $type->id;
+        }
+
+        $sports = [
+            ['name' => 'Ski', 'sport_type' => $typeIds['Hiver']],
+            ['name' => 'Snowboard', 'sport_type' => $typeIds['Hiver']],
+            ['name' => 'VTT', 'sport_type' => $typeIds['Été']],
+        ];
+
+        foreach ($sports as $sport) {
+            Sport::firstOrCreate(
+                ['name' => $sport['name']],
+                [
+                    'icon_collective' => 'icon.png',
+                    'icon_prive' => 'icon.png',
+                    'icon_activity' => 'icon.png',
+                    'icon_selected' => 'icon.png',
+                    'icon_unselected' => 'icon.png',
+                    'sport_type' => $sport['sport_type'],
+                ]
+            );
+        }
     }
 }
