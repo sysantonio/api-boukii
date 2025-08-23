ddfff3590b609ba88a0d45acf7bd4ee40cda86e9
sysantonio <andf92@gmail.com>
2025-08-23 12:01:56 +0200

    test(auth): add basic auth specs and jest config

--- front/jest.config.cjs
+++ front/jest.config.cjs
@@ -1,17 +1,7 @@
-/** @type {import('jest').Config} */
 module.exports = {
   preset: 'jest-preset-angular',
-  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html)$'
-      }
-    ]
-  },
+  testEnvironment: 'jsdom',
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   moduleNameMapper: {
     '^@core/(.*)$': '<rootDir>/src/app/core/$1',
@@ -21,15 +11,5 @@ module.exports = {
     '^@state/(.*)$': '<rootDir>/src/app/state/$1',
     '^@assets/(.*)$': '<rootDir>/src/assets/$1',
     '^@environments/(.*)$': '<rootDir>/src/environments/$1'
-  },
-  moduleFileExtensions: ['ts', 'js', 'html', 'json'],
-  collectCoverageFrom: [
-    'src/app/**/*.ts',
-    '!src/main.ts',
-    '!src/environments/**',
-    '!src/polyfills.ts'
-  ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text-summary'],
-  testMatch: ['**/?(*.)+(spec).ts']
+  }
 };
